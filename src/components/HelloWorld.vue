152ee83febc5b6b26b2709ee87f966c4f9da38fb
Marc Kunst <m.kunst@socialblue.com>
2022-03-02 12:19:58 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Mark Smark
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style lang="scss" scoped>
+p {
+    a {
+      color: #ff0000;
+      font-size: 1.5rem;
+    }
+}
+</style>
