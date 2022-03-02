4a80bdd9d102550a2e2f6ae8e4869fc3a9dc5dad
m.de.nijs <m.de.nijs@socialblue.com>
2022-03-02 17:13:56 +0100

    first portion of the monthly data component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import PageHeader from './components/page-header/PageHeader.vue'
+import MonthlyData from './components/monthly-data/MonthlyData.vue'
 </script>
 
 <template>
   <PageHeader msg="Unibrow Insights" />
+  <MonthlyData />
 </template>
 
 <style lang="scss">
-@import "scss/global.scss";
-
+  @import "scss/global.scss";
 </style>
---
+++ src/components/monthly-data/MonthlyData.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="monthly-data">
+        <h3 class="monthly-data__title">
+            Monthly Forecasted Data
+        </h3>
+
+        <div class="monthly-data__inner-wrapper">
+            <div class="monthly-data__block monthly-data__revenue">
+                <h4 class="monthly-data__block-title">
+                    Revenue
+                </h4>
+
+                <p class="monthly-data__block-items">
+                    <span class="monthly-data__block-items-main monthly-data__block-revenue-month">
+                        {{ RevenueMonth }}
+                    </span>
+                    <br>
+                    <span class="monthly-data__block-items-secondary monthly-data__block-revenue-month-forecast">
+                        {{ RevenueMonthForecast }}
+                    </span>
+                </p>
+            </div>
+
+            <div class="monthly-data__block monthly-data__cost">
+                <h4 class="monthly-data__block-title">
+                    Cost
+                </h4>
+            </div>
+
+            <div class="monthly-data__block monthly-data__margin">
+                <h4 class="monthly-data__block-title">
+                    Margin
+                </h4>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            RevenueMonth: "25,968.92",
+            RevenueMonthForecast: "61,925.88"
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "../../scss/global.scss",
+    "monthly-data";
+</style>
