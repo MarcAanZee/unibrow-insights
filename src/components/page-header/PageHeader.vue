36ce95b0efb1989a4220fd0f6cc37b3e3881a1e7
Marc Kunst <m.kunst@socialblue.com>
2022-03-02 15:39:01 +0100

    first setup and style

---
+++ src/components/page-header/PageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="page-header">
+    <h1 class="page-header__title">page-header</h1>
+
+    <div class="page-header__button">
+        <router-link class="page-header__button-label" to="/page-header">Show page-header</router-link>
+    </div>
+  </main>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+@import "../../scss/global.scss",
+"page-header";
+</style>
