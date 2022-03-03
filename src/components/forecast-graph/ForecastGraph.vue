3115f720d9ef0c47ff9e1eced366d17a0fbd7610
Marc Kunst <m.kunst@socialblue.com>
2022-03-03 13:21:29 +0100

    Added smooth graph lines

--- src/components/forecast-graph/ForecastGraph.vue
+++ src/components/forecast-graph/ForecastGraph.vue
@@ -18,7 +18,18 @@ export default defineComponent({
       datasets: [
         {
           data: [3500, 3800, 4100, 3900, 3800, 3200, 3800, 3200, 3500, 3900, 3600, 3200, 3000, 3300, 3200, 2900, 3100, 3000, 3900, 4100, 3200, 3600, 3800, 3100, 3500, 3600, 3800, 4000, 3500, 3700, 3200],
-          backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+          backgroundColor: ['#ff696440'],
+          fill: true,
+          borderColor: "$color-black",
+          lineTension: 0.4,
+        },
+
+                {
+          data: [350, 380, 410, 390, 300, 320, 380, 320, 300, 390, 360, 320, 3000, 3300, 3200, 2900, 3100, 3000, 3900, 4100, 3200, 3600, 3800, 3100, 3500, 3600, 3800, 4000, 3500, 3700, 3200],
+          backgroundColor: ['#0000ff'],
+          fill: true,
+          borderColor: "$color-black",
+          lineTension: 0.4,
         },
       ],
     };
