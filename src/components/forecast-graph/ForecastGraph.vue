116e09dfc0548a270ca49d66ec3e8cd812098216
Marc Kunst <m.kunst@socialblue.com>
2022-03-03 10:53:00 +0100

    Added chartist files

---
+++ src/components/forecast-graph/ForecastGraph.vue
@@ -0,0 +1,27 @@
+<template>
+    <chartist
+        ratio="ct-major-second"
+        type="Line"
+        :data="chartData"
+        :options="chartOptions" >
+    </chartist>
+</template>
+
+<script>
+export default {
+    data: {
+        chartData: {
+            labels: ["A", "B", "C"],
+            series:[[1, 3, 2], [4, 6, 5]]
+        },
+        chartOptions: {
+            lineSmooth: false
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "../../scss/global.scss",
+    "forecast-graph";
+</style>
